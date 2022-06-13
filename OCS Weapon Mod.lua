523de835825574d3d063a387c9390bab3e72784c
bignooboxploits <107345975+sprmy@users.noreply.github.com>
2022-06-12 20:28:16 -0700

    Add files via upload

---
+++ OCS Weapon Mod.lua
@@ -0,0 +1,11 @@
+local gun = game:GetService("Players").LocalPlayer.Backpack:GetChildren()
+local b = (unpack(gun))
+local gun2 = require(b.Config)
+
+--MAKE SURE TO ONLY HAVE 1 GUN IN YOUR INVENTORY FOR THIS TO WORK. RECOMMEND ANTI-MAT
+
+gun2["Firerate"] = 475
+gun2["Recoil"] = { .05, .05, .05, .05 } 
+--gun2["FireMode"] = Auto 
+--gun2["DamageIndicationNumber"] = false
+--gun2["ReloadSpeed"] = 0 
