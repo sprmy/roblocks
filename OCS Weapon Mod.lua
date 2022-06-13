dd3a8ade457581fd366dcdc251a3e6448d02c68f
bignooboxploits <107345975+sprmy@users.noreply.github.com>
2022-06-12 20:30:45 -0700

    Update OCS Weapon Mod.lua

--- OCS Weapon Mod.lua
+++ OCS Weapon Mod.lua
@@ -4,8 +4,8 @@ local gun2 = require(b.Config)
 
 --MAKE SURE TO ONLY HAVE 1 GUN IN YOUR INVENTORY FOR THIS TO WORK. RECOMMEND ANTI-MAT (OR IT MODS A RANDOM GUN, WILL FIX ONCE I GAIN MORE KNOWLEDGE)
 
-gun2["Firerate"] = 475
-gun2["Recoil"] = { .05, .05, .05, .05 } 
+gun2["Firerate"] = 50000
+gun2["Recoil"] = { .01, .01, .01, .01 } 
 gun2["FireMode"] = Auto 
 gun2["DamageIndicationNumber"] = false
 gun2["ReloadSpeed"] = 0 
