b381157dfc4dd9a4ae90049f4efe54c8de16cdf5
bignooboxploits <107345975+sprmy@users.noreply.github.com>
2022-06-14 14:56:13 -0700

    Update OCS Weapon Mod.lua

--- OCS Weapon Mod.lua
+++ OCS Weapon Mod.lua
@@ -2,7 +2,7 @@ local gun = game:GetService("Players").LocalPlayer.Backpack:GetChildren()
 local b = (unpack(gun))
 local gun2 = require(b.Config)
 
---MAKE SURE TO ONLY HAVE 1 GUN IN YOUR INVENTORY FOR THIS TO WORK. RECOMMEND ANTI-MAT (OR IT MODS A RANDOM GUN, WILL FIX ONCE I GAIN MORE KNOWLEDGE)
+--MAKE SURE TO ONLY HAVE 1 GUN IN YOUR INVENTORY FOR THIS TO WORK. RECOMMEND ANTI-MAT
 
 gun2["Firerate"] = 50000
 gun2["Recoil"] = { .01, .01, .01, .01 } 
